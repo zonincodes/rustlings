4f1f4b8d617492b8b70837a5fd57a005d366be46
zonin <business.kariukimartin@gmail.com>
2024-03-07 19:33:31 +0300

    completed strings

--- exercises/09_strings/strings3.rs
+++ exercises/09_strings/strings3.rs
@@ -3,21 +3,24 @@
 // Execute `rustlings hint strings3` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
-
+use std::str;
 fn trim_me(input: &str) -> String {
     // TODO: Remove whitespace from both ends of a string!
-    ???
+    let string = input.trim();
+    string.to_string()
 }
 
 fn compose_me(input: &str) -> String {
     // TODO: Add " world!" to the string! There's multiple ways to do this!
-    ???
+    let mut string = input.to_string();
+    string.push_str(" world!");
+    string
 }
 
 fn replace_me(input: &str) -> String {
     // TODO: Replace "cars" in the string with "balloons"!
-    ???
+    input.to_string();
+    input.replacen("cars", "balloons", 1)
 }
 
 #[cfg(test)]
